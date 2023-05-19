dfaae4b0752a188f1b5a2c9783181c3d43c9411f
Yurii Rybachok <drummer2009@ukr.net>
2023-05-19 16:14:28 +0300

    create project & setup

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <button class="bg-violet-500 p-5 rounded-md">Hey</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+
+</style>
