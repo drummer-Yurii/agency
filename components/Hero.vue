12a2c8108048ea8e8c6006664e7b9aaf2f141e1c
Yurii Rybachok <drummer2009@ukr.net>
2023-05-20 12:24:22 +0300

    add btn for chat

--- components/Hero.vue
+++ components/Hero.vue
@@ -42,6 +42,15 @@
                 </div>
             </div>
             <div class="basis-1/2 overflow-hidden relative hidden lg:block">
+                <div class="absolute top-5 right-[15%] group cursor-pointer">
+                    <div class="h-20 w-16 bg-amber-400 rounded-full relative">
+                        <img 
+                            src="/arrow.png" alt="Arrow Icon"
+                            class="w-full absolute -left-8 top-3 group-hover:translate-x-4 transition-all duration-300" 
+                        />
+                    </div>
+                    <p class="font-bold text-xl">Let's chat <br />with us!</p>
+                </div>
                 <img src="/home.png" alt="Image of a woman" class="object-cover h-[100$]" />
             </div>
         </div>
