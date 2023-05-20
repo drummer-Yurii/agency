49a60684716e25088a48ec14ce342ef1de6ad3b1
Yurii Rybachok <drummer2009@ukr.net>
2023-05-20 09:58:22 +0300

    tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,29 @@ module.exports = {
     "./app.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["inter", "sans-serif"],
+      },
+      spacing: {
+        desktop: '100px',
+        mobile: '10px'
+      },
+      animation: {
+        fade: 'fade 0.4s ease-in-out',
+        slideDown: 'slideDown 1s ease-in-out',
+      },
+      keyframes: {
+        fade: {
+          '0%': {scale: 0, opacity: 0},
+          '100%': {scale: 1, opacity: 1},
+        },
+        slideDown: {
+          "0%": { transform: "translateY(-70px)", opacity: 0},
+          "100%": { transform: "translateY(0)", opacity: 1},
+        }
+      }
+    },
   },
   plugins: [],
 }
